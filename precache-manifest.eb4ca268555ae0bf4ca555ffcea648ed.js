2b32dfa21765f6b8af893db410160890bb5cd8d3
Marcin Raszkiewicz <raszkiewicz.m@gmail.com>
2020-04-19 22:12:45 +0200

    Updates

---
+++ precache-manifest.eb4ca268555ae0bf4ca555ffcea648ed.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170a66ec061aa0c7fe16d9515c573098",
+    "url": "/github_card_app/index.html"
+  },
+  {
+    "revision": "6be2106789a297894182",
+    "url": "/github_card_app/static/css/2.228a363c.chunk.css"
+  },
+  {
+    "revision": "087b91a8100a0d828816",
+    "url": "/github_card_app/static/css/main.4c032a81.chunk.css"
+  },
+  {
+    "revision": "6be2106789a297894182",
+    "url": "/github_card_app/static/js/2.cd866bc2.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/github_card_app/static/js/2.cd866bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "087b91a8100a0d828816",
+    "url": "/github_card_app/static/js/main.5c0bfcba.chunk.js"
+  },
+  {
+    "revision": "e8b738b42874fd2fa2f3",
+    "url": "/github_card_app/static/js/runtime-main.a91c5124.js"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/github_card_app/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/github_card_app/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/github_card_app/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/github_card_app/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/github_card_app/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/github_card_app/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/github_card_app/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/github_card_app/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/github_card_app/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/github_card_app/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/github_card_app/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/github_card_app/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/github_card_app/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/github_card_app/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/github_card_app/static/media/fa-solid-900.ec763292.svg"
+  }
+]);
